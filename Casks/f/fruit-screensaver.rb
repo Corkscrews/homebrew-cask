10d05de1d6813cd7176c750e6241f1c6503160ac
Pedro Paulo Amorim <pepa.amorim@gmail.com>
2025-11-25 00:35:32 +0000

    fruit-screensaver 1.3.2

--- Casks/f/fruit-screensaver.rb
+++ Casks/f/fruit-screensaver.rb
@@ -1,6 +1,6 @@
 cask "fruit-screensaver" do
-  version "1.3.0"
-  sha256 "c020eadf79270f2f846b67176a43bde8a113b8b91e9cd770ad03c8d65a427c49"
+  version "1.3.2"
+  sha256 "cc46ea8b2a5b079544177595f954e04cebc037cf26fb12f30bcbadcc375ef662"
 
   url "https://github.com/Corkscrews/fruit/releases/download/#{version}/Fruit.saver.tar.gz"
   name "Fruit Screensaver"
